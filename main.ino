7357c1f7583606e985591fb5881ac8dc4e6586c2
Dhiaan Dave <120334591+the-X-alien@users.noreply.github.com>
2025-10-18 18:35:51 -0700

    Refactor servo initialization and control logic

--- main.ino
+++ main.ino
@@ -1,21 +1,18 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 
-// Motor control pins
 #define IN1_PIN 12
 #define IN2_PIN 14
 #define IN3_PIN 26
 #define IN4_PIN 27
 
-// Servo control pin
 #define SERVO_PIN 32
 
-// Create a Servo object
 Servo myServo;
 
 void setup() {
   Serial.begin(9600);
-  // Set motor pins as outputs
+
   pinMode(IN1_PIN, OUTPUT);
   pinMode(IN2_PIN, OUTPUT);
   pinMode(IN3_PIN, OUTPUT);
@@ -24,12 +21,12 @@ void setup() {
   digitalWrite(IN2_PIN, LOW);
   digitalWrite(IN3_PIN, LOW);
   digitalWrite(IN4_PIN, LOW);
-  // Attach the servo to the pin
-  myServo.setPeriodHertz(50);          // Optional: set frequency to 50Hz (standard for servos)
-  myServo.attach(SERVO_PIN, 500, 2400); // Attach with min/max pulse width in microseconds
 
-  myServo.write(45); // Center the servo
-  stopMotors();      // Stop motors initially
+  myServo.setPeriodHertz(50);
+  myServo.attach(SERVO_PIN, 500, 2400);
+
+  myServo.write(45);
+  stopMotors();
 }
 
 void loop() {
@@ -45,14 +42,13 @@ void loop() {
       case 'd': turnRight(); break;
       case 'x': stopMotors(); break;
 
-      case 'j': myServo.write(45); break;    // Turn servo left
-      case 'l': myServo.write(135); break;   // Turn servo right
-      case 'k': myServo.write(90); break;    // Center the servo
+      case 'j': myServo.write(45); break;
+      case 'l': myServo.write(135); break;
+      case 'k': myServo.write(90); break;
     }
   }
 }
 
-// Motor control functions
 void moveForward() {
   digitalWrite(IN1_PIN, HIGH);
   digitalWrite(IN2_PIN, LOW);
